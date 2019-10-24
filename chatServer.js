a1d23d8418ddebd3a0c43e8d2f88a29a8529de7c
sgc87 <54932585+sgc87@users.noreply.github.com>
2019-10-24 05:54:05 -0400

    Update chatServer.js

--- chatServer.js
+++ chatServer.js
@@ -61,31 +61,31 @@ function bot(data, socket, questionNum) {
   } else if (questionNum == 1) {
     if (input === 'no') {
       answer = 'Oh, I see! Well, that is what I am here for! To help you choose a ride ;)';
-      waitTime = 5000;
+      waitTime = 7000;
     } else {
       answer = 'Okay, a ' + input + ' you said? Sweet, but today, you will find an even sweeter ride. I can look into the trade-in value for you if you as well!';
-      waitTime = 5000;
+      waitTime = 8000;
     }
     question = 'Do you worry about hot weather or cold weather more?'; // load next question
   } else if (questionNum == 2) {
     if (input.toLowerCase() === 'cold' || input.toLowerCase() === 'cold weather') {
       answer = 'Cold weather I see. In that case, I recommend getting heated seats, usually a $500 option by itself. I also recommend getting AWD or 4WD if it snows where you live. ';
       temp = 'cold';
-      waitTime = 5000;
+      waitTime = 10000;
     } else if (input.toLowerCase() === 'hot' || input.toLowerCase() === 'hot weather') {
       answer = 'Hot weather I see. In that case, I recommend getting cooled seats. However, this is usually part of a premium package as cooled seats are currently available with leather seats. ';
       temp = 'hot';
-      waitTime = 5000;
+      waitTime = 10000;
     }
     question = 'How many people are you looking to seat excluding yourself? For passenger cars, the limit is 7. ';
   } else if (questionNum == 3) {
-    if (input === 0 || input === 1 || input === 2) {
+    if (input == 0 || input == 1 || input == 2) {
       answer = 'Okay, in that case, I would recommend a 2 door car like a coupe. ';
       waitTime = 5000;
-    } else if (input === 3 || input === 4) {
+    } else if (input == 3 || input == 4) {
       answer = 'Okay, in that case, I would recommend any car with four doors. ';
       waitTime = 5000;
-    } else if (input === 5 || input === 6 || input === 7) {
+    } else if (input == 5 || input == 6 || input == 7) {
       answer = 'Okay, in that case, I would recommend an SUV, crossover, or a minivan. ';
       waitTime = 5000;
     } else {
